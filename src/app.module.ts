ed5929144da29c99b57d42bcd05ec6abb3b8d22c
Jian Han <jianhan@mail.com>
2019-03-04 22:11:14 +1000

    [feat] setup mongo db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigService } from './config/config.service';
 import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost/gfu'),
-    AuthModule,
-    UsersModule,
-    ConfigModule,
-  ],
+  imports: [TypeOrmModule.forRoot(), AuthModule, UsersModule, ConfigModule],
   controllers: [AppController],
-  providers: [AppService, ConfigService],
+  providers: [AppService],
 })
 export class AppModule {}
